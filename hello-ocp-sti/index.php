3cc0fb48264e013d5a0896bd9f26540c1e2722b2
kofibreik <marimar.alonso@gmail.com>
2019-05-22 18:17:32 +0200

    hello-ocp-sti

---
+++ hello-ocp-sti/index.php
@@ -0,0 +1,15 @@
+<html>
+	<head>
+		<title> hello-ocp-sti </title>
+	</head>
+
+
+<h1>hello-ocp-sti</h1>
+<br>
+Pod hostname: <?php echo getenv('HOSTNAME')?>
+<br>
+Pod ip: <?php echo $_SERVER['SERVER_ADDR']?>
+<br>
+<br>
+phpinfo: <?php phpinfo()?>
+</html>
